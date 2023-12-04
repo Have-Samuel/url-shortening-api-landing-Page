4c26e14080180059ca81ef113dcbbd49455a3b66
Have Samuel <samiehave9@gmail.com>
2023-12-04 17:05:00 +0300

    Writing the POPUP functioning

---
+++ script.js
@@ -0,0 +1,8 @@
+// Navbar Menu
+const menuPop = document.querySelector('.humburger');
+const navbar = document.querySelector('.navbar');
+
+menuPop.addEventListener('click', () => {
+  menuPop.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
