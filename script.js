f13cc2c8c6b2271e7440b70f4c9bcb4a9a5693ed
Have Samuel <samiehave9@gmail.com>
2023-12-04 19:42:25 +0300

    Creating the shorteniing functions

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ menuPop.addEventListener('click', () => {
   menuPop.classList.toggle('active');
   navbar.classList.toggle('active');
 });
+
+// Shortening URL
+const form = document.querySelector('form');
+const btn = document.querySelector('#shorten');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('.input');
+  const url = input.value;
+
+  if (!url) {
+    input.classList.add('error');
+  } else {
+    input.classList.remove('error');
+    input.value = '';
+    shortenUrl(url);
+  }
+});
