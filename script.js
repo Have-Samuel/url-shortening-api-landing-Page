7410585bf55cf1268d45561b9a35fef59232b017
Have Samuel <samiehave9@gmail.com>
2023-12-04 19:43:10 +0300

    Deleting

--- script.js
+++ script.js
@@ -10,17 +10,3 @@ menuPop.addEventListener('click', () => {
 // Shortening URL
 const form = document.querySelector('form');
 const btn = document.querySelector('#shorten');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const input = document.querySelector('.input');
-  const url = input.value;
-
-  if (!url) {
-    input.classList.add('error');
-  } else {
-    input.classList.remove('error');
-    input.value = '';
-    shortenUrl(url);
-  }
-});
