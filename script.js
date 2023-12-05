b6a40e77f3e2d982ba79f4d15d48724ac7271e35
Have Samuel <samiehave9@gmail.com>
2023-12-05 15:17:48 +0300

    Fetching the Data from hte API

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { async } = require("q");
+
 // Navbar Menu
 const menuPop = document.querySelector('.humburger');
 const navbar = document.querySelector('.navbar');
@@ -8,5 +10,30 @@ menuPop.addEventListener('click', () => {
 });
 
 // Shortening URL
-const form = document.querySelector('form');
-const btn = document.querySelector('#shorten');
+const form = document.querySelector('#form-js');
+const input = document.querySelector('.input');
+const display = document.querySelector('.display');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const url = input.value;
+
+  shortenUrl(url);
+});
+
+async function shortenUrl(url) {
+  // Try Block will execute when we have a
+  // successful shortening of the LINK
+  try {
+    const res = await fetch(`https://cleanuri.com/api/v1/shorten?url=${url}`)
+    const data = res.json();
+
+    // Create a new Variable and assign a class
+    const newUrl = document.createElement('div')
+    newUrl.classList.add('item')
+  }
+  //  CATCH block will handle the errors
+  catch (error) {
+
+  }
+}
