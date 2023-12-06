031b1f3a4ce1781b22a20e2c278f76588e4754bc
Have Samuel <samiehave9@gmail.com>
2023-12-06 10:50:53 +0300

    Fetching

--- script.js
+++ script.js
@@ -12,40 +12,55 @@ const form = document.querySelector('#form-js');
 const input = document.querySelector('.input');
 const display = document.querySelector('.display');
 
+const data = {
+  url: input.value,
+};
+
+// Create a new Variable and assign a class
+const newUrl = document.createElement('div');
+newUrl.classList.add('item');
+newUrl.innerHTML = `
+ <p>${data.display.short_link}</p>
+ <button class='newUrl-btn'>Copy</button>
+ `;
+
+display.prepend(newUrl);
+const copyBtn = document.querySelector('.newUrl-btn');
+copyBtn.addEventListener('click', () => {
+  // Adding functionality to the COPY, so we use this below
+  // PreviousElementSibling gives us the paragragh
+  navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+});
+// Reseting the Input field
+input.value = '';
+
+postData('https://cleanuri.com/api/v1/shorten', data)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 // Fetching the Data
-async function shortenUrl(url) {
+async function postData(url = '', data = {}) {
   // Try Block will execute when we have a
   // successful shortening of the LINK
-  try {
-    const res = await fetch(`https://cleanuri.com/api/v1/shorten?url=${url}`);
-    const data = res.json();
 
-    // Create a new Variable and assign a class
-    const newUrl = document.createElement('div');
-    newUrl.classList.add('item');
-    newUrl.innerHTML = `
-    <p>${data.display.short_link}</p>
-    <button class='newUrl-btn'>Copy</button>
-    `;
-
-    display.prepend(newUrl);
-    const copyBtn = document.querySelector('.newUrl-btn');
-    copyBtn.addEventListener('click', () => {
-      // Adding functionality to the COPY, so we use this below
-      // PreviousElementSibling gives us the paragragh
-      navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
-    });
-    // Reseting the Input field
-    input.value = '';
-    //  CATCH block will handle the errors
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const url = input.value;
 
-  shortenUrl(url);
+  postData(url);
 });
