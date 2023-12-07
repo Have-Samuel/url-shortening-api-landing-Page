dd12c8bb823edeafed97b875c89949eb69c672fd
Have Samuel <samiehave9@gmail.com>
2023-12-07 16:18:11 +0300

    Corrections

--- script.js
+++ script.js
@@ -15,39 +15,39 @@ const display = document.querySelector('.display');
 
 // Posting Data to the API
 async function postData(url) {
-  try{
-  const res = await fetch(`https://cleanuri.com/api/v1/shorten?url=${url}`, {
-    mode: 'no-cors',
-    credentials: 'include',
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ url }),
-  });
+  try {
+    const res = await fetch(`https://cleanuri.com/api/v1/shorten?url=${url}`, {
+      mode: 'no-cors',
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ url }),
+    });
 
-  const data = await res.json();
-  // Create a new Variable and assign a class
-  const newUrl = document.createElement('div');
-  newUrl.classList.add('item');
-  newUrl.innerHTML = `
+    const data = await res.json();
+    // Create a new Variable and assign a class
+    const newUrl = document.createElement('div');
+    newUrl.classList.add('item');
+    newUrl.innerHTML = `
    <p>${data.display.result_url}</p>
    <button class='newUrl-btn'>Copy</button>
    `;
 
-  display.prepend(newUrl);
-  const copyBtn = document.querySelector('.newUrl-btn');
-  copyBtn.addEventListener('click', () => {
+    display.prepend(newUrl);
+    const copyBtn = document.querySelector('.newUrl-btn');
+    copyBtn.addEventListener('click', () => {
     // Adding functionality to the COPY, so we use this below
     // PreviousElementSibling gives us the paragragh
-    navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
-  });
-  // Reseting the Input field
-  input.value = '';
-} catch(err){
-  console.log(err);
+      navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+    });
+    // Reseting the Input field
+    input.value = '';
+  } catch (err) {
+    console.log(err);
+  }
 }
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const url = input.value;
