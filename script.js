2cec9b9040fe2acf85b0876511049e244ca56f5d
Have Samuel <samiehave9@gmail.com>
2023-12-07 11:57:48 +0300

    Fetcing Data from the API

--- script.js
+++ script.js
@@ -13,25 +13,33 @@ const form = document.querySelector('#form-js');
 const input = document.querySelector('.input');
 const display = document.querySelector('.display');
 
-// Fetching Data from the API
-
-
 // Posting Data to the API
-const postData = async (url) => {
-  const res = await fetch('https://cleanuri.com/api/v1/shorten', {
+// const postData = async (url) => {
+//   const res = await fetch(`https://cleanuri.com/api/v1/shorten?url=${url}`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//     body: JSON.stringify({ url }),
+//   });
+//   const data = await res.json();
+//   console.log(data);
+// https://cleanuri.com/api/v1/shorten
+async function postData(url) {
+  const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`, {
     method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ url }),
+    // headers: {
+    //   'Content-type': 'application/json',
+    // },
+    // body: JSON.stringify({ url }),
   });
+
   const data = await res.json();
-  console.log(data);
   // Create a new Variable and assign a class
   const newUrl = document.createElement('div');
   newUrl.classList.add('item');
   newUrl.innerHTML = `
-   <p>${data.result_url}</p>
+   <p>${data.display.result_url}</p>
    <button class='newUrl-btn'>Copy</button>
    `;
 
@@ -44,7 +52,7 @@ const postData = async (url) => {
   });
   // Reseting the Input field
   input.value = '';
-};
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -52,21 +60,3 @@ form.addEventListener('submit', (e) => {
 
   postData(url);
 });
-
-// // Create a new Variable and assign a class
-// const newUrl = document.createElement('div');
-// newUrl.classList.add('item');
-// newUrl.innerHTML = `
-//  <p>${data.display.short_link}</p>
-//  <button class='newUrl-btn'>Copy</button>
-//  `;
-
-// display.prepend(newUrl);
-// const copyBtn = document.querySelector('.newUrl-btn');
-// copyBtn.addEventListener('click', () => {
-//   // Adding functionality to the COPY, so we use this below
-//   // PreviousElementSibling gives us the paragragh
-//   navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
-// });
-// // Reseting the Input field
-// input.value = '';
