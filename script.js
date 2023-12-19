9a9c6edef196cd0663c2041b5d999d772037a64d
Have Samuel <samiehave9@gmail.com>
2023-12-19 16:16:28 +0300

    Delete the Unwanted code

--- script.js
+++ script.js
@@ -11,35 +11,6 @@ const form = document.querySelector('#form-js');
 const input = document.querySelector('.input');
 const display = document.querySelector('.display');
 
-// Create the DOM function to display the data
-// function displayData(data, url) {
-//   // console.log(data, 'from displayData', data.url);
-//   const div = document.createElement('div');
-//   div.classList.add('display-data');
-//   div.innerHTML = `
-//     <p class="wording">${data}</p>
-//     <hr>
-//     <p class="shortened-url">${url}</p>
-//     <button class="btn-Url">Copy</button>
-//   `;
-//   display.append(div);
-//   // Copy to Clipboard
-//   const copy = document.querySelector('.btn-Url');
-//   copy.addEventListener('click', () => {
-//     navigator.clipboard.writeText(copy.previousElementSibling.textContent);
-//     copy.textContent = 'Copied';
-//     copy.style.backgroundColor = '#2ecc71';
-//     copy.style.color = '#fff';
-//     setTimeout(() => {
-//       copy.textContent = 'Copy';
-//       copy.style.backgroundColor = '#fff';
-//       copy.style.color = '#000';
-//     }, 2000);
-//     console.log(copy.previousElementSibling.textContent);
-//   });
-//   input.value = '';
-// }
-
 // Posting Data to the API
 async function postData(url) {
   console.log(url);
