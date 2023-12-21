b58388a658f39945bd872524e2f3dcef6d68dd39
Have Samuel <samiehave9@gmail.com>
2023-12-21 17:19:02 +0300

    Deleting

--- script.js
+++ script.js
@@ -35,7 +35,12 @@ function displayData(data) {
   const div = document.createElement('div');
   div.classList.add('display-data');
   // Show the input url and the shortened url to the DOM
-  // If statement to display the shortened url
+  div.innerHTML = `
+<p class="wording">${data}</p>
+<hr>
+<p class="shortened-url">${data.result_url}</p>
+<button class="btn-Url">Copy</button>
+`;
 
   display.append(div);
   // Copy to Clipboard
