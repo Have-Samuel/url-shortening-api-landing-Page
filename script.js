e1d7c14759cf2976fdc932ca41d6b03940a77bda
Have Samuel <samiehave9@gmail.com>
2024-01-05 17:02:33 +0300

    Looping thru the copy btns

--- script.js
+++ script.js
@@ -56,6 +56,14 @@ function displayData(data, url) {
     navigator.clipboard.writeText(copy.previousElementSibling.textContent);
   });
   input.value = '';
+  // When copy buttons is clicked, change the text to copied
+  copy.forEach((copy) => copy.addEventListener('click', () => {
+  copy.addEventListener('click', () => {
+    copy.textContent = 'Copied';
+    copy.style.backgroundColor = '#3a3054';
+    copy.style.color = '#fff';
+  });
+}))
 }
 
 // If the form has no input, display an error message
